a6e72e7271f5e0a8c92d2a1da30d99b6d5976cb2
Колобова Ксения Евгеньевна <shrimpksko@gmail.com>
2022-04-06 14:49:53 +0700

    Добавила решение задания №2

--- proxy-wrapper/proxy-wrapper.js
+++ proxy-wrapper/proxy-wrapper.js
@@ -1,5 +1,43 @@
 // Задание №2.
 // Напишите proxy-обертку для объекта, которая умеет достраивать недостающие промежуточные вложенные узлы в объекте.
 // Сделайте так, чтобы этот proxy-объект корректно конвертировался в JSON формат, при помощи метода JSON.stringify.
+"use strict";
 
+const fruitProxy = object => new Proxy(object, proxyHandler);
 
+const proxyHandler = {
+    get(target, prop) {
+        if (prop === 'toJSON') return () => target;
+
+        prop in target
+            ? target[prop] = fruitProxy(target[prop])
+            : target[prop] = fruitProxy({});
+
+        return Reflect.get(target, prop)
+    }
+}
+
+const proxy = fruitProxy({
+    apple: { count: 7 },
+    nectarine: {
+        count: 7,
+        country: {
+            name: 'China',
+            region: 'East Asia'
+        }
+    }
+});
+
+proxy.orange.count = 30;
+proxy.melon.country.name = 'Turkey';
+proxy.watermelon.country.region = 'West Asia';
+
+console.log(JSON.stringify(proxy));
+// output:
+// {
+// "apple":{"count":7},
+// "nectarine":{"count":7,"country":{"name":"China","region":"East Asia"}},
+// "orange":{"count":30},
+// "melon":{"country":{"name":"Turkey"}},
+// "watermelon":{"country":{"region":"West Asia"}}
+// }
